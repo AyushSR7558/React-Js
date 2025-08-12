311f021fa2ae6b748c046abcd0eedc2313934236
AyushSR7558 <ayushraut7558@gmail.com>
2025-08-12 23:37:29 +0530

    Your commit message here

--- namasateReact/App.js
+++ namasateReact/App.js
@@ -1,2 +1,17 @@
 import React from "react";
-import ReactDom from "react-dom"
+import ReactDOM from "react-dom/client"
+
+//createElement is the Object and not the html tag
+const heading = React.createElement("h1",{id : "heading" , color : "red"}, "Namasate!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// In the render stage the object will be converted into the html tag
+root.render(heading);
+
+// JSX is not the html
+// JSX is different and HTML is different 
+const JSXHeading = <h1 id = "heading" color = "red">Namasate React using JSX</h1>
+
+console.log(heading);
+console.log(JSXHeading);
